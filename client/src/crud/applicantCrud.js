907da95fd8147a6627d029278ffe6ffd5b2e7527
Calvin Rafferty <calvinhobbes352@gmail.com>
2017-12-07 08:40:19 -0800

    applicant crud

---
+++ client/src/crud/applicantCrud.js
@@ -0,0 +1,39 @@
+const baseUrl = (path = "") => `http://localhost:4040/api/applicant/${path}`;
+
+const create = data => {
+  return fetch(baseUrl(), {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(resonse => response.json())
+    .catch(error => console.log(error));
+};
+
+const getById = id => {
+  return fetch(baseUrl(id), {
+    method: "POST",
+    body: JSON.stringify(),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(resonse => response.json())
+    .catch(error => console.log(error));
+};
+
+const update = (id, data) => {
+  return fetch(baseUrl(id), {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(resonse => response.json())
+    .catch(error => console.log(error));
+};
+
+export default { create, getById, update };
